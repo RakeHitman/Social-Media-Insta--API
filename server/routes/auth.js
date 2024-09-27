71f90bb9d8e786177ac384d2e7916c03892e9859
Aman Bisht <flex4104@gmail.com>
2024-09-27 15:53:49 +0530

    User authentication [JWT is to be added]

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 import express from "express";
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 const router = express.Router();
 
 // REGISTER
@@ -16,6 +17,31 @@ router.post("/register", async (req, res) => {
         const newUser = new User({...req.body , password : hashedPassword});
         const saveUser = await newUser.save();
         res.status(200).json(saveUser);
+        console.log(saveUser);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+// LOGIN
+router.post("/login" , async (req , res) => {
+    try {
+        let user;
+        if ( req.body.email ) {
+            user = await User.findOne({email : req.body.email});
+        } else if ( req.body.username ) {
+            user = await User.findOne({username : req.body.username});
+        } else if (!user) {
+            res.status(404).json("User not found")
+        }
+
+        const match = await bcrypt.compare(req.body.password , user.password);
+        if(!match) {
+            res.status(401).json("Wrong Credentials !")
+        }
+
+        res.status(200).json(user)
+        console.log("A user just logged in !")
     } catch (error) {
         res.status(500).json(error);
     }
